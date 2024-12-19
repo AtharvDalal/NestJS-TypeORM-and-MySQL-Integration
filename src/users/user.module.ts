dad74673256077d1df990322d77053cb0c22db55
Atharv Dalal <sangamdalal24@gmail.com>
2024-12-19 15:20:57 +0530

    Typeorm

--- src/users/user.module.ts
+++ src/users/user.module.ts
@@ -3,9 +3,11 @@ import { UserController } from './user.controller';
 import { UserService } from './user.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from 'src/typeORM/enitites/User';
+import { Profile } from 'src/typeORM/enitites/Profile';
+import { userPost } from 'src/typeORM/enitites/Post';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User])],
+  imports: [TypeOrmModule.forFeature([User, Profile, userPost])],
   providers: [UserService],
   controllers: [UserController],
 })
