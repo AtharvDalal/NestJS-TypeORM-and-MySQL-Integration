565ae97484cde53f8b6ee3743fdf51a46323a09b
Atharv Dalal <sangamdalal24@gmail.com>
2024-12-19 15:20:47 +0530

    types

--- src/types/types.ts
+++ src/types/types.ts
@@ -2,3 +2,21 @@ export type CreateUserParams = {
   username: string;
   password: string;
 };
+export type UpdateUserParams = {
+  username: string;
+  password: string;
+};
+
+export type CreateProfileParams = {
+  name: string;
+  surname: string;
+  email: string;
+  age: number;
+  address: string;
+};
+
+export type userpostParams = {
+  title: string;
+
+  desc: string;
+};
